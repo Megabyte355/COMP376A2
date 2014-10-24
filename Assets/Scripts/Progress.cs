60f4a54c8d4f42107ff1f9d19c66754aad6e6083
Gary Chang <gary.chang.355@gmail.com>
2014-10-24 00:05:39 -0400

    Added basic UI for score and progress

---
+++ Assets/Scripts/Progress.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Progress : MonoBehaviour
+{
+    [SerializeField]
+    Camera cam;
+    [SerializeField]
+    int topOffSetPixels;
+    [SerializeField]
+    int leftOffSetPixels;
+
+    int progress = 0;
+    TextMesh textMesh;
+
+    void Start()
+    {
+        transform.position = cam.ScreenToWorldPoint(new Vector3(leftOffSetPixels, Screen.height - topOffSetPixels, 10));
+        textMesh = gameObject.GetComponent<TextMesh>();
+    }
+
+    void Update()
+    {
+        textMesh.text = "Progress: " + progress + "%";
+    }
+
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField]
+    Camera cam;
+    [SerializeField]
+    int topOffSetPixels;
+    [SerializeField]
+    int leftOffSetPixels;
+
+    int score = 0;
+    TextMesh textMesh;
+    
+    void Start()
+    {
+        transform.position = cam.ScreenToWorldPoint(new Vector3(leftOffSetPixels, Screen.height - topOffSetPixels, 10));
+        textMesh = gameObject.GetComponent<TextMesh>();
+    }
+
+    void Update()
+    {
+        textMesh.text = "Score: " + score;
+    }
+
+    public void AddPoints(int pts)
+    {
+        score += pts;
+    }
+}
